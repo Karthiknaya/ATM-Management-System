75dc7ab6488d3b18af82832fe19cea12fc263496
Karthik Nayak <108059989+Karthiknaya@users.noreply.github.com>
2024-02-01 12:27:46 +0530

    Update Conn.java

--- ATM Bank Management System/src/atm/bank/management/system/Conn.java
+++ ATM Bank Management System/src/atm/bank/management/system/Conn.java
@@ -7,7 +7,7 @@ public class Conn {
     Statement s;
     public Conn(){
         try {
-            c= DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","kart@1234@");
+            c= DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","xxxxxxxxxx");
             s=c.createStatement();
             
         } catch (Exception e){
